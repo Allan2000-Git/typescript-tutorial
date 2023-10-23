d2d4ccb059c60f6c63ff29b7e3b7796892d6fa48
Allan Joston Fernandes <allanfernandes459@gmail.com>
2023-10-23 17:31:55 +0530

    ts-day2

--- pureTS/dist/index.js
+++ pureTS/dist/index.js
@@ -1,9 +1,21 @@
 "use strict";
 // console.log("typescript");
+// class User{
+//     public email:string
+//     password:string // by default public
+//     readonly city: string = "mangalore" // can be accessed but not overridden
+//     constructor(email:string, password:string){
+//         this.email = email
+//         this.password = password
+//     }
+// }
 class User {
     constructor(email, password) {
         this.email = email;
         this.password = password;
+        this.city = "mangalore"; // can be accessed but not overridden
     }
+    get userEmail() { return this.email; }
 }
 const allan = new User("allan@gmail.com", "12345abcd");
+// allan.city = "mangalore"
--- pureTS/src/index.ts
+++ pureTS/src/index.ts
@@ -1,12 +1,22 @@
 // console.log("typescript");
 
+// class User{
+//     public email:string
+//     password:string // by default public
+//     readonly city: string = "mangalore" // can be accessed but not overridden
+//     constructor(email:string, password:string){
+//         this.email = email
+//         this.password = password
+//     }
+// }
+
 class User{
-    email:string
-    password:string
-    constructor(email:string, password:string){
-        this.email = email
-        this.password = password
+    readonly city: string = "mangalore" // can be accessed but not overridden
+    constructor(public email:string, public password:string){
     }
+
+    get userEmail():string { return this.email; }
 }
 
 const allan = new User("allan@gmail.com","12345abcd")
+// allan.city = "mangalore"
